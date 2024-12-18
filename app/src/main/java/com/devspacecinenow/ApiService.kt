237b4d86761a01f15425561e8755116a45c52996
brunomagga <brunomagga2007@gmail.com>
2024-12-18 18:05:04 -0300

    test

---
+++ app/src/main/java/com/devspacecinenow/ApiService.kt
@@ -0,0 +1,13 @@
+package com.devspacecinenow
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiService {
+
+    @GET("now_playing?language=en-US&page=1")
+    fun getNowPLayingMovies() : Call<MovieResponse>
+
+    @GET("top_rated?language=en-US&page=1")
+    fun getTopRatedMovies() : Call<MovieResponse>
+}
---
+++ app/src/main/java/com/devspacecinenow/MovieDto.kt
@@ -0,0 +1,18 @@
+package com.devspacecinenow
+
+import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.Serializable
+
+
+ data class MovieDto (
+     val id: Int,
+     val title: String,
+     val overview: String,
+     @SerializedName("poster_path")
+     val postPath: String,
+ ){
+     val posterFullPath: String
+         get() = "https://image.tmdb.org/t/p/w300$postPath"
+
+
+ }
